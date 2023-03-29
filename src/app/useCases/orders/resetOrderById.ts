534cf7548354aa131a136843bb936e891d38813c
adrianomolin <adriano.molin@hotmail.com>
2023-03-29 08:59:42 -0300

    fix: resetOrder syntax

--- src/app/useCases/orders/resetOrderById.ts
+++ src/app/useCases/orders/resetOrderById.ts
@@ -5,9 +5,9 @@ import { Order } from '../../models/Order';
 export async function resetOrderById(req: Request, res: Response) {
   try {
     const { orderId } = req.params;
-    const orders = await Order.findByIdAndUpdate(orderId, { archived: true });
+    const order = await Order.findByIdAndUpdate(orderId, { archived: true });
 
-    res.status(201).json(orders);
+    res.status(201).json(order);
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
