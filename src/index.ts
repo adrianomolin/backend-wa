f9069f9e14f6b551ec78af80ae7ac59baf2a4e8b
Adriano Molin <adriano.molin@hotmail.com>
2024-03-26 19:59:20 -0300

    Fix env files not being loaded

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,28 @@
+import 'dotenv/config';
+
 import http from 'http';
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
 import { Server } from 'socket.io';
-
 import { router } from './router';
 import cors from './app/middleware/cors';
+import { env } from './config/env';
 
 const app = express();
 const server = http.createServer(app);
 export const io = new Server(server);
 
-dotenv.config();
-
 mongoose.set('strictQuery', true);
-mongoose.connect(process.env.DB_URI || '');
+mongoose.connect(env.dbURI);
 
 app.use(cors);
 app.use(express.json());
 app.use(router);
 
-app.listen(3000);
+
+app.listen(3000).on('listening', () => {
+  console.log('Server running on port 3000!');
+});
+
 
 export default app;
