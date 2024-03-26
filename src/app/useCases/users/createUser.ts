d233483d0a657ac7ba5a7747dc119cb2431a30a1
Adriano Molin <adriano.molin@hotmail.com>
2024-03-26 20:06:54 -0300

    Implement multi-tenancy to user entity

--- src/app/useCases/users/createUser.ts
+++ src/app/useCases/users/createUser.ts
@@ -1,42 +1,84 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcryptjs';
-import { User } from '../../models/User';
+import { v4 as uuidv4 } from 'uuid';
+import { getDBModel, switchDB } from '../../../tenant/utils/switchDb';
+import { jwtSign } from '../../utils/jwtSign';
+import { schemas, tenantSchemas } from '../../../tenant/utils/schemas';
 
 export async function createUser(req: Request, res: Response) {
   try {
     const {
       name,
       email,
-      password,
-      role
+      password
     } = req.body;
+
+    let {
+      role,
+    } = req.body;
+
+    let orgId = req.user?.orgId;
 
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
 
-    if (!name || !email || !password) {
-      res.status(400).json({
-        'error': 'Name, email and password fields are required!',
-      });
+    try {
+      if (!orgId) role = 'ADMIN';
+
+      orgId = await addUserToOrg({ orgId, email });
+    } catch (err) {
+      if (!(err instanceof Error)) return;
+
+      if (err.message === 'email') {
+        return res.status(409).json({
+          'error': 'This email is already registered in an organization.',
+        });
+      }
     }
 
-    if (req.headers['demo'] === 'true') return res.sendStatus(201).json({
-      name,
-      email,
-      password: passwordHash,
-      role
-    })
+    const tenant = await switchDB(orgId, schemas);
+    const model = await getDBModel(tenant, 'User');
 
-    const user = await User.create({
+    const user = await model.create({
       name,
       email,
       password: passwordHash,
-      role
+      role,
+      orgId,
     });
 
-    res.status(201).json(user);
+    const accessToken = jwtSign(user.id, user.orgId, user.permissions);
+
+    return res.json({ accessToken }).status(201);
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
   }
+}
+
+async function addUserToOrg({ orgId, email, name }: { orgId: string, email: string, name?: string }) {
+  const orgTenant = await switchDB('tenant', tenantSchemas);
+  const users = await getDBModel(orgTenant, 'users');
+
+  const isEmailAlreadyTaken = await users.findOne({ email });
+
+  if (isEmailAlreadyTaken) {
+    throw new Error('email');
+  }
+
+  if (!orgId) orgId = uuidv4();
+
+  const organizations = await getDBModel(orgTenant, 'organizations');
+
+  await organizations.create({
+    orgId,
+    name
+  });
+
+  await users.create({
+    orgId,
+    email: email
+  });
+
+  return orgId;
 }
--- src/app/useCases/users/deleteUser.ts
+++ src/app/useCases/users/deleteUser.ts
@@ -1,18 +1,37 @@
 import { Request, Response } from 'express';
-
-import { User } from '../../models/User';
+import { getDBModel, switchDB } from '../../../tenant/utils/switchDb';
+import { schemas } from '../../../tenant/utils/schemas';
 
 export async function deleteUser(req: Request, res: Response) {
   try {
+    const tenant = req.tenant;
     const { userId } = req.params;
 
-    if (req.headers['demo'] === 'true') return res.sendStatus(204);
+    const model = await getDBModel(tenant, 'User');
 
-    await User.findByIdAndDelete(userId);
+    const user = await model.findById(userId);
+
+    if (!user) {
+      return res.sendStatus(404);
+    }
+
+    await model.findByIdAndDelete(userId);
+    await deleteUserFromTenantRecords(user.orgId, user.email);
 
     res.sendStatus(204);
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
+  }
+}
+
+async function deleteUserFromTenantRecords(orgId: string, email: string) {
+  try {
+    const orgTenant = await switchDB('tenant', schemas);
+    const model = await getDBModel(orgTenant, 'users');
+
+    await model.findOne({ email: email, orgId }).deleteOne();
+  } catch (err) {
+    console.log(err);
   }
 }
